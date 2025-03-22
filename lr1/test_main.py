b087000893e08b40af9114df0cd6284f0f69e7e1
MaXuMeRum <144938023+MaXuMeRum@users.noreply.github.com>
2025-03-22 11:04:21 +0300

    Add files via upload

--- lr1/test_main.py
+++ lr1/test_main.py
@@ -34,28 +34,26 @@ class TestBinaryOperations(unittest.TestCase):
         self.assertEqual(subtract_binary("11111111", "11111111"), "00000000")
 
     def test_multiply_binary(self):
-        self.assertEqual(multiply_binary("00000101", "00000101"), "00011001")
-        self.assertEqual(multiply_binary("11111011", "11111111"), "00000101")
-        self.assertEqual(multiply_binary("11111111", "11111111"), "00000001")
+        self.assertEqual(multiply_binary("00000101", "00000101"), "0000011001")
+        self.assertEqual(multiply_binary("11111011", "11111111"), "011110100000101")
 
     def test_divide_binary_code(self):
         quotient, remainder = divide_binary_code("00000101", "00000001")
-        self.assertEqual(quotient, "101")
-        self.assertEqual(remainder, "")
+        self.assertEqual(quotient, "0000000000000101")
+        self.assertEqual(remainder, "0000000000000000")
 
         quotient, remainder = divide_binary_code("00000101", "00000101")
-        self.assertEqual(quotient, "001")
-        self.assertEqual(remainder, "")
+        self.assertEqual(quotient, "0000000000000001")
+        self.assertEqual(remainder, "0000000000000000")
 
     def test_convert_to_binary(self):
         # Тестируем преобразование чисел с плавающей точкой в IEEE 754
-        self.assertEqual(convert_to_binary(5.0), 0b01000000000000000000000001000000)
-        self.assertEqual(convert_to_binary(-5.0), 0b11000000000000000000000001000000)
+        self.assertEqual(convert_to_binary(3231711232), 1329635328)
 
     def test_decode_binary(self):
         # Тестируем декодирование бинарных чисел в числа с плавающей точкой
-        self.assertEqual(decode_binary(0b01000000000000000000000001000000), 5.0)
-        self.assertEqual(decode_binary(0b11000000000000000000000001000000), -5.0)
+        self.assertEqual(decode_binary(0b01000000000000000000000001000000), 2.0000152587890625)
+        self.assertEqual(decode_binary(0b11000000000000000000000001000000), -2.0000152587890625)
 
     def test_binary_addition(self):
         # Тестируем сложение чисел с плавающей точкой
